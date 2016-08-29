a7d43f81da860d71521883917a5209cb1b1852a6
mriehl <max@flatmap.ninja>
2016-08-29 10:18:18 +0200

    bug report

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := """coursier-test"""
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "com.github.tomakehurst" % "wiremock" % "2.1.11"
+
---
+++ src/main/scala/com/example/Hello.scala
@@ -0,0 +1,9 @@
+package com.example
+
+import com.github.tomakehurst.wiremock.WireMockServer
+
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
